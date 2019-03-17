2874fb1c2e78f2482e1009278acac5b3b487a3b2
jakubchaberka <48645501+jakubchaberka@users.noreply.github.com>
2019-03-17 17:13:43 +0100

    Wersja1

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+
+            Console.Write("Podaj swoje imię, proszę: ");
+            string imie = Console.ReadLine();
+
+            Console.WriteLine($"Cześć {imie}!!");
+        }
+    }
+}
