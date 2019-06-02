865a655a5444e0ee3330a246c0d6dda4523e2c17
jakubchaberka <48645501+jakubchaberka@users.noreply.github.com>
2019-06-02 14:27:46 +0200

    master

---
+++ GraGUI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace GraGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNowaGra = new System.Windows.Forms.Button();
+            this.groupBoxLosuj = new System.Windows.Forms.GroupBox();
+            this.labelOd = new System.Windows.Forms.Label();
+            this.textBoxOd = new System.Windows.Forms.TextBox();
+            this.labelDo = new System.Windows.Forms.Label();
+            this.textBoxDo = new System.Windows.Forms.TextBox();
+            this.buttonLosuj = new System.Windows.Forms.Button();
+            this.groupBoxLosuj.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonNowaGra
+            // 
+            this.buttonNowaGra.Location = new System.Drawing.Point(12, 12);
+            this.buttonNowaGra.Name = "buttonNowaGra";
+            this.buttonNowaGra.Size = new System.Drawing.Size(75, 23);
+            this.buttonNowaGra.TabIndex = 0;
+            this.buttonNowaGra.Text = "Nowa gra";
+            this.buttonNowaGra.UseVisualStyleBackColor = true;
+            this.buttonNowaGra.Click += new System.EventHandler(this.buttonNowaGra_Click);
+            // 
+            // groupBoxLosuj
+            // 
+            this.groupBoxLosuj.Controls.Add(this.buttonLosuj);
+            this.groupBoxLosuj.Controls.Add(this.textBoxDo);
+            this.groupBoxLosuj.Controls.Add(this.labelDo);
+            this.groupBoxLosuj.Controls.Add(this.textBoxOd);
+            this.groupBoxLosuj.Controls.Add(this.labelOd);
+            this.groupBoxLosuj.Location = new System.Drawing.Point(12, 63);
+            this.groupBoxLosuj.Name = "groupBoxLosuj";
+            this.groupBoxLosuj.Size = new System.Drawing.Size(290, 100);
+            this.groupBoxLosuj.TabIndex = 1;
+            this.groupBoxLosuj.TabStop = false;
+            this.groupBoxLosuj.Text = "Losowanie";
+            this.groupBoxLosuj.Visible = false;
+            // 
+            // labelOd
+            // 
+            this.labelOd.AutoSize = true;
+            this.labelOd.Location = new System.Drawing.Point(7, 20);
+            this.labelOd.Name = "labelOd";
+            this.labelOd.Size = new System.Drawing.Size(58, 13);
+            this.labelOd.TabIndex = 0;
+            this.labelOd.Text = "Zakres od:";
+            // 
+            // textBoxOd
+            // 
+            this.textBoxOd.Location = new System.Drawing.Point(72, 12);
+            this.textBoxOd.Name = "textBoxOd";
+            this.textBoxOd.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOd.TabIndex = 1;
+            // 
+            // labelDo
+            // 
+            this.labelDo.AutoSize = true;
+            this.labelDo.Location = new System.Drawing.Point(7, 63);
+            this.labelDo.Name = "labelDo";
+            this.labelDo.Size = new System.Drawing.Size(58, 13);
+            this.labelDo.TabIndex = 2;
+            this.labelDo.Text = "Zakres do:";
+            // 
+            // textBoxDo
+            // 
+            this.textBoxDo.Location = new System.Drawing.Point(72, 56);
+            this.textBoxDo.Name = "textBoxDo";
+            this.textBoxDo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDo.TabIndex = 3;
+            // 
+            // buttonLosuj
+            // 
+            this.buttonLosuj.Location = new System.Drawing.Point(190, 35);
+            this.buttonLosuj.Name = "buttonLosuj";
+            this.buttonLosuj.Size = new System.Drawing.Size(75, 23);
+            this.buttonLosuj.TabIndex = 2;
+            this.buttonLosuj.Text = "Wylosuj";
+            this.buttonLosuj.UseVisualStyleBackColor = true;
+            this.buttonLosuj.Click += new System.EventHandler(this.buttonLosuj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 469);
+            this.Controls.Add(this.groupBoxLosuj);
+            this.Controls.Add(this.buttonNowaGra);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxLosuj.ResumeLayout(false);
+            this.groupBoxLosuj.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNowaGra;
+        private System.Windows.Forms.GroupBox groupBoxLosuj;
+        private System.Windows.Forms.Button buttonLosuj;
+        private System.Windows.Forms.TextBox textBoxDo;
+        internal System.Windows.Forms.Label labelDo;
+        private System.Windows.Forms.TextBox textBoxOd;
+        private System.Windows.Forms.Label labelOd;
+    }
+}
+
